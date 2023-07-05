c7483d86212dc5dbe68e2c8acdb9b8b818a4cca8
justinschoi93 <justinschoi93@gmail.com>
2023-07-05 14:17:03 -0700

    psuedocode

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// keydown event listener 
+// if correct, replace textContet, _ to correct letter
+// when all _ have been replaced, game is won, winner display message is displayed, timer is stopped
+// when timer reaches 0, game is lost, loser display message is desplayed, timer is stopped
+// start button is also a reset button
+// win and loss counts stored in localStorage
+// 
